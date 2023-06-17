3b6f0e2807c8086ec97083c3eb4b601db11ddbb1
PIV-y <iv.proshin@yandex.ru>
2023-06-17 17:19:39 +0500

    com2

--- src/main/java/com/piv/spring/Person.java
+++ src/main/java/com/piv/spring/Person.java
@@ -2,16 +2,27 @@ package com.piv.spring;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
-//    public Person(Pet pet) {
-//        this.pet = pet;
-//    }
-
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+        System.out.println("sureName has been set");
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+        System.out.println("age has been set");
+    }
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
-
     public void callYourPet() {
         System.out.println("Hello, my lovely pet!");
         pet.say();

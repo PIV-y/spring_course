15083daa0718236da7e46fe1cd47596e0bfa9fe0
PIV-y <iv.proshin@yandex.ru>
2023-06-17 15:41:29 +0500

    com1

--- src/main/java/com/piv/spring/Person.java
+++ src/main/java/com/piv/spring/Person.java
@@ -3,7 +3,12 @@ package com.piv.spring;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        this.pet = pet;
+//    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
